ef8889258743d5cddbf871ab780a5f4db5414744
Mikko Vartiainen <mvartiainen@gmail.com>
2014-02-02 19:54:55 +0200

    configuration file, unicode fixes, continuous update

--- homesensor-lcd.py
+++ homesensor-lcd.py
@@ -4,31 +4,41 @@
 from pylcdsysinfo import BackgroundColours, COL2LEFT, TextColours, TextAlignment, TextLines, LCDSysInfo
 import urllib
 import json
+import time
+import unicodedata
+import ConfigParser
 
 class Screen():
     def __init__(self):
         self.lcd = LCDSysInfo()
         self.lcd.clear_lines(TextLines.ALL, BackgroundColours.BLACK)
         self.lcd.dim_when_idle(False)
-        self.lcd.set_brightness(255)
-        self.lcd.save_brightness(127, 255)
+        self.lcd.set_brightness(127)
+        self.lcd.save_brightness(127, 127)
 
     def clear(self):
         self.lcd.clear_lines(TextLines.ALL, BackgroundColours.BLACK)
 
-    def printc(self, data):
-        for line,sensor in enumerate(data):
-            try:
-                value = sensor['value']
-            except:
-                value = str(sensor['value'])
+    def printc(self, data, sensors_to_display):
+        line = 1
+        for key in sensors_to_display:
+            for sensor in data:
+                if sensor['id'] == key:
+                    value = str(sensor['value'])
+                    txt = unicode(sensor['name'] + "\t " + value + "" + sensor['unit'])
+                    txt = txt.replace(u"\u00B0", "^")
+                    txt = unicodedata.normalize('NFKD', txt).encode('ascii', 'ignore')
+                    print(txt)
+                    self.lcd.display_text_on_line(line, txt, False, TextAlignment.LEFT, TextColours.LAVENDER)
+                    line +=1
 
-            txt = sensor['name'] + ": " + value + "" + sensor['unit']
-            print(txt)
-            self.lcd.display_text_on_line(line+1, txt, False, None, TextColours.GREEN)
-
-def read_config():
-    return {'url': 'http://localhost/homesensor/sensor.php'}
+def read_config(filename):
+    config = ConfigParser.ConfigParser()
+    config.read(filename)
+    conf = {}
+    conf['url'] = config.get("Server", "url")
+    conf['sensors'] = json.loads(config.get("Display", "sensors"))
+    return conf
 
 def get_values(url):
     f = urllib.urlopen(url)
@@ -36,8 +46,17 @@ def get_values(url):
     f.close()
     return json.loads(data)
 
-if __name__ == "__main__":
+def main():
     screen = Screen()
-    config = read_config()
-    content = get_values(config['url'])
-    screen.printc(content)
+    config = read_config("config.ini")
+    while True:
+        try:
+            content = get_values(config['url'])
+            screen.printc(content, config['sensors'])
+        except:
+            pass
+        time.sleep(30)
+
+
+if __name__ == "__main__":
+    main()
